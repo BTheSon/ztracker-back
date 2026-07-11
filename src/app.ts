e27fdb7e1dbd96ef713c7e571ef774d00cbbfa13
Bui_The_Son <buitheson201@gmail.com>
2026-07-11 18:27:17 +0700

    add cors and add / endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,13 @@ import { initRoutes } from './routes';
 const app: Application = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://ztracker-back.onrender.com",
+        "http://localhost:5173/",
+    ],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,9 @@ import apiRouter from "./api.route";
 
 
 export const initRoutes = (app: Application) => {
+    app.get("/", (_, res) => {
+        res.send("OK");
+    });
     app.use('/api', apiRouter);
     app.use('/worker', apiRouter);
 }
