bda4d6e923e9d9343b7ad3045e7d438622317f96
Bui_The_Son <buitheson201@gmail.com>
2026-07-12 15:07:10 +0700

    quên add route cho worker:)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Application } from "express";
 import apiRouter from "./api.route";
 import pushRoutes from "./push.routes";
-
+import workerRoutes from "./worker.route";
 
 export const initRoutes = (app: Application) => {
     app.get("/", (_, res) => {
         res.send("OK");
     });
     app.use('/api', apiRouter);
-    app.use('/worker', apiRouter);
+    app.use('/worker', workerRoutes);
     app.use('/push', pushRoutes);
 }
