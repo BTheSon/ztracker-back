407e757c1b395311fac8d16f765aca660d5020e5
Bui_The_Son <buitheson201@gmail.com>
2026-07-15 16:41:34 +0700

    fix: endpoint

--- src/socket.ts
+++ src/socket.ts
@@ -13,8 +13,23 @@ export const initSocket = (server: HttpServer) => {
 
     io.on('connection', (socket: Socket) => {
         console.log(`User connected: ${socket.id}`);
-
+        if (socket.handshake.auth?.type === 'zalo-worker') {
+            // 1. Log ra server
+            console.log(`Zalo Worker connected: ${socket.id}`);
+        
+            // 2. Bắn thông báo (Broadcast) tới toàn bộ các thiết bị (trình duyệt, app) đang kết nối
+            io.emit('worker_status', { 
+                status: 'online', 
+                message: 'Zalo Worker đã kết nối thành công!' 
+            });
+        }
         socket.on('disconnect', () => {
+            if (socket.handshake.auth?.type === 'zalo-worker') {
+                io.emit('worker_status', { 
+                    status: 'offline', 
+                    message: 'Zalo Worker đã mất kết nối!' 
+                });
+            }
             console.log(`User disconnected: ${socket.id}`);
         });
     });
